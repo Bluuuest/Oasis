53f09b258d78d1779d9c7b1d37377b9b1e7f4c69
Bao Pham <blu@Baos-MacBook-Pro.local>
2026-08-13 14:07:43 -0700

    menu update

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-class Oasis:
+def main():
+    email = 'empty'
+    answer1 = input('Login as Guest or create account?; (1 = Guest, 2 = Create)      ')
+    if answer1 == 1:
+        email = 'Guest'
+    elif answer1 == 2:
+        email = input()
+        password = input()
+        age = int(input())
+
+    print('[', '-' * 10, 'Task Manager', '-' * 10, ']')
+
+    while True:
+        print(f'Welcome, {email}')
+        print('     1. Pull Up Tasks Recorded')
+        print('     2. Track Productivity')
+        print('     3. Log Out')
+        if email == True:
+            print('     4. Premium Membership')
+        answer2 = input('Type a number.;     ')
+
+class Task:
     def __init__(self, name, time_hours, exp):
         self.name = name
         self.time_hours = time_hours
@@ -9,12 +30,12 @@ class Oasis:
     and exp is below 0
     '''
     def error(self, checker):
-        if self.time_hours < 0 or self.exp < 0:
+        if self.time_hours <= 0 or self.exp <= 0:
             print('Error; Value cannot be negative')
-        elif self.time_hours > 100 or self.exp > 100:
-            time_hours += 25
-            exp += 25
-
 
 if __name__ == '__main__':
-    task1 = Oasis('Money Laundering', 730, -28)
+    task1 = Task('Money Laundering', 730, -28)
+    task2 = Task('Backflip', 0.1, 50)
+    task3 = Task('Apple Picking', 2, 25)
+
+    main()
