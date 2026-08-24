f12424b8d67b30691e95a934d569bb0f289de23f
Bao Pham <blu@Baos-MacBook-Pro.local>
2026-08-24 13:09:55 -0700

    save changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
     email = 'empty'
-    answer1 = input('Login as Guest or create account?; (1 = Guest, 2 = Create)      ')
+    answer1 = int(input('Login as Guest or create account?; (1 = Guest, 2 = Create) '))
     if answer1 == 1:
         email = 'Guest'
     elif answer1 == 2:
-        email = input()
-        password = input()
-        age = int(input())
+        email = str(input('Please type in your email;   '))
+        password = str(input('Please type in a password;    '))
+        age = int(input('Please type in your age, number only;  '))
 
     print('[', '-' * 10, 'Task Manager', '-' * 10, ']')
 
@@ -24,12 +24,13 @@ class Task:
         self.name = name
         self.time_hours = time_hours
         self.exp = exp
+        self.error()
 
     '''
     this should print an error if amount of hours
     and exp is below 0
     '''
-    def error(self, checker):
+    def error(self):
         if self.time_hours <= 0 or self.exp <= 0:
             print('Error; Value cannot be negative')
 
