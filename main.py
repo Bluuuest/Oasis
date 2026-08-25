522ca97e4e085363a27188c3ba9bc13289c4eb69
Bao Pham <blu@Baos-MacBook-Pro.local>
2026-08-25 13:00:47 -0700

    solved the printing issue for listing out a new task in the taskList

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ here, we are defining the new main function,
 to manage the program's menu and logistics
 managing the user's info
 '''
-
 '''
-asks user for input on guest vs account
+main function asks user for input on guest vs account
 '''
 def main():
-    email = 'empty'
+
+    taskList = []
+    jobList = []
+    errandList = []
+    email = ' '
+    newName = ' '
+    newHours = 0
+    newTask = ' '
+    chooseType = 0
+
     answer1 = int(input('Login as Guest or create account?; (1 = Guest, 2 = Create) '))
     if answer1 == 1:
         email = 'Guest'
@@ -16,32 +24,62 @@ def main():
         email = str(input('Please type in your email;   '))
         password = str(input('Please type in a password;    '))
         age = int(input('Please type in your age, number only;  '))
+    else:
+        print('Invalid data given, please try again.')
+        return
 
-    print('[', '-' * 10, 'Task Manager', '-' * 10, ']')
+    print('                   [', '-' * 10, 'Task Manager', '-' * 10, ']')
 
-    newName = ' '
-    newHours = 0
-
-    if answer1 == 1 or answer1 == 2:
+    while True:
+        print('=' * 75)
         print(f'Welcome, {email}')
         print('     1. Pull Up Tasks Recorded')
         print('     2. Track Productivity')
-        print('     3. Log Out')
+        print('     3. Job Book')
+        answer2 = int(input('Type a number.;     '))
 
-    answer2 = int(input('Type a number.;     '))
+        if answer2 == 1:
+            print('Here are all of your current tasks.;')
+            print(taskList)
+            print('-' * 15, 'Maximum of 5 Tasks', '-' * 15)
+            print('What would you like to do?;')
+            print('     1. Add a new task')
+            print('     2. Overwrite a task')
+            print('     3. Delete a task')
+            print('     4. Go back!')
+            print('     5. Examine a task')
 
-    if answer2 == 1:
-        print(Task.printTasks)
-    elif answer2 == 2:
-        newName = str(input('What was the name of this task?    '))
-        newHours = float(input('How many hours did you spent on this task today? #    '))
-        newExp = newHours * 5
-        newTask = Task(newName, newHours, newExp)
-        print(f'You have logged; "{newName}" with {newHours} in hours, gaining {newExp} exp in return.')
-        print(newTask)
-    elif answer2 == 3:
-        print('Goodbye!')
-        return
+            option1Answer = int(input('Please type a number.;   '))
+
+            if option1Answer == 1:
+                newName = str(input("Type in the task's name.   "))
+                newHours = int(input('How many hours did you spent on this task today?  '))
+                newExp = float(newHours * 5)
+                newTask = Task(newName, newHours, newExp)
+                taskList.append(newTask)
+                print(f'Successfully appended the new "{newName}" task to your list.')
+                continue
+        
+        elif answer2 == 2:
+            newName = str(input('What was the name of this task?    '))
+            chooseType = int(input('What type of task was it? [task = 1, job = 2, errand = 3]   '))
+            newHours = float(input('How many hours did you spent on this task today?    '))
+            newExp = newHours * 5
+            newTask = Task(newName, newHours, newExp)
+
+            if chooseType == 1:
+                taskList.append(newTask)
+            elif chooseType == 2:
+                jobList.append(newTask)
+            elif chooseType == 3:
+                errandList.append(newTask)
+
+            print(f'You have logged; "{newName}" with {newHours} in hours, gaining {newExp} exp in return.')
+            continue
+        elif answer2 == 3:
+            print('Here are all available jobs.;')
+        elif not isinstance(answer2, int) or answer2 > 5:
+            print('Invalid data given, please try again.')
 
 class Task:
     def __init__(self, name, hoursSpent, exp):
@@ -57,15 +95,10 @@ class Task:
     def error(self):
         if self.hoursSpent <= 0 or self.exp <= 0:
             print('Error; Value cannot be negative')
-
-    def printTasks(self):
-        print(Task())
+    
+    def __repr__(self):
+        printable = self.__class__.__name__
+        return f'{printable} (Name; {self.name}, Hours; {self.hoursSpent}), XP; {self.exp}'
 
 if __name__ == '__main__':
-    task1 = Task('Money Laundering', 730, -28)
-    task2 = Task('Backflip', 0.1, 50)
-    task3 = Task('Apple Picking', 2, 25)
-    task4 = Task('Package Shipping', 0.5, 25)
-    taskList = [task1, task2, task3, task4]
-
     main()
